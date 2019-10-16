ffa8bb8ad88186d70f301ce6ff8a0958b47b3ce0
Total-Hydrographic <55672165+Total-Hydrographic@users.noreply.github.com>
2019-10-16 16:28:20 +1100

    yep

---
+++ pointclouds/3dModel/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 100000000,
+    "boundingBox": {
+        "lx": 269036.91,
+        "ly": 5774772.411,
+        "lz": -9.690000000000002,
+        "ux": 269550.88,
+        "uy": 5775286.381,
+        "uz": 504.28000000003029
+    },
+    "tightBoundingBox": {
+        "lx": 269036.91,
+        "ly": 5774772.411,
+        "lz": -9.690000000000002,
+        "ux": 269550.87,
+        "uy": 5775199.219,
+        "uz": 0.36099999999999979
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 4.45111083984375,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
